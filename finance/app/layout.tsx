d534a23804631165cc21ec147971a1a845495f4d
Meet <meet.dev@projecthumancity.com>
2025-02-20 21:32:46 -0700

    Post

--- finance/app/layout.tsx
+++ finance/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { QueryProviders } from "@/providers/query-provider";
+import { SheetProvider } from "@/providers/sheet-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,10 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <QueryProviders>{children}</QueryProviders>
+          <QueryProviders>
+            <SheetProvider />
+            {children}
+          </QueryProviders>
         </body>
       </html>
     </ClerkProvider>
